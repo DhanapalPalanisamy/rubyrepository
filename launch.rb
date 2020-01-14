9a23c8acea5c94ba1d5f177751baa13a737a9903
dhanapalpalanisamy <dhanapal.rp@gmail.com>
2020-01-14 11:57:12 +0530

    udated

--- launch.rb
+++
@@ -1,5 +0,0 @@
-puts "Please enter your name"
-name = gets.chomp
-puts "Hello, #{name}! I'm Ruby!"
-
-
--- watirdemo.rb
+++
@@ -1,5 +0,0 @@
-require 'watir-webdriver'
-
-
-driver=Watir::Browser.new :chrome
-driver.goto "https://www.google.com"
